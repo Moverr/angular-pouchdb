7242698659107811cdf7f4b0e9631db36f4ed216
Muyinda Rogers <moverr@gmail.com>
2020-01-30 10:17:56 +0300

    Adding Pouch Db Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PouchdbComponent } from './pouchdb/pouchdb.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PouchdbComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pouchdb/pouchdb.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pouchdb',
+  templateUrl: './pouchdb.component.html',
+  styleUrls: ['./pouchdb.component.css']
+})
+export class PouchdbComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
